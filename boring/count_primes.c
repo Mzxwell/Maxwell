20cd874aa0c043edeaa99e584e8894a1d1ad5f17
Maxwell <1979829742@qq.com>
2023-07-10 11:10:29 +0800

    prime

--- boring/count_primes.c
+++ boring/count_primes.c
@@ -1,15 +1,9 @@
-//
-// Created by Jinx on 2023/7/10.
-//
 #include "stdlib.h"
 #include "stdio.h"
 #include "stdbool.h"
-#include "sys/time.h"
 int main() {
-    struct timeval start,end;
     int n, count = 0;
     scanf("%d", &n);
-    gettimeofday(&start,NULL);
     bool *a = (bool *) malloc(n + 1);
     for (int i = 0; i < n + 1; ++i) a[i] = 1;
     if(n>=2) {
@@ -17,6 +11,4 @@ int main() {
         for (int i = 1; i < n + 1; i+=2) count += a[i];
     }
     printf("%d\n", count);
-    gettimeofday(&end,NULL);
-    printf("%f",(double)(end.tv_sec - start.tv_sec) + (double)(end.tv_usec - start.tv_usec) / 1000000.0);
 }
