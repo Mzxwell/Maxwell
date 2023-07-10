c9aecfa8bc9de452aab0e8e539ee501015412061
Maxwell <1979829742@qq.com>
2023-07-10 12:46:43 +0800

    Quine

---
+++ boring/Quine.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    char *program = "#include <stdio.h>%c%cint main() {%c    char *program = %c%s%c;%c    printf(program, 10, 10, 10, 34, program, 34, 10, 10, 10);%c}";
+    printf(program, 10, 10, 10, 34, program, 34, 10, 10, 10);
+}
