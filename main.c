19c5036dbe31c89d5f5bd8d9a69bfc6fa103b3cd
Maxwell <1979829742@qq.com>
2024-03-12 21:28:13 +0800

    .561

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main() {
     printf("Hello, World!\n");
-    double const a= .561,b=21.;
-    printf("%f",a+b);
+    double const a= .561,b=21.,c0df11fd2$5_5= 0xap10;
+    printf("%f,%f",a+b,c0df11fd2$5_5);
     return 0;
 }
